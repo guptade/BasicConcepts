5bd51772a034afdad67607f64a0fea6fc1825a9b
Devesh Gupta <deveshgupta179@gmail.com>
2017-04-13 20:14:05 +0530

    builder pattern

---
+++ designpatterns/src/main/java/in/deveshgupta/tutorials/designpatterns/creational/builder/Waiter.java
@@ -0,0 +1,20 @@
+package in.deveshgupta.tutorials.designpatterns.creational.builder;
+
+class Waiter {
+	private PizzaBuilder pizzaBuilder;
+
+	public void setPizzaBuilder(PizzaBuilder pb) {
+		pizzaBuilder = pb;
+	}
+
+	public Pizza getPizza() {
+		return pizzaBuilder.getPizza();
+	}
+
+	public void constructPizza() {
+		pizzaBuilder.createNewPizzaProduct();
+		pizzaBuilder.buildDough();
+		pizzaBuilder.buildSauce();
+		pizzaBuilder.buildTopping();
+	}
+}
