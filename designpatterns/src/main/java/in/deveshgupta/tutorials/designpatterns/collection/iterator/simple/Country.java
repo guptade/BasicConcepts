36dd6a148473ba5013095700a228962eaf2bde69
Devesh Gupta <deveshgupta179@gmail.com>
2017-04-13 22:21:40 +0530

    iterator pattern - simple

---
+++ designpatterns/src/main/java/in/deveshgupta/tutorials/designpatterns/collection/iterator/simple/Country.java
@@ -0,0 +1,39 @@
+package in.deveshgupta.tutorials.designpatterns.collection.iterator.simple;
+
+public class Country {
+
+	private String code;
+	private String name;
+
+	public Country() {
+		super();
+	}
+
+	public Country(String code, String name) {
+		super();
+		this.code = code;
+		this.name = name;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Country [code=" + code + ", name=" + name + "]";
+	}
+
+}
---
+++ designpatterns/src/main/java/in/deveshgupta/tutorials/designpatterns/collection/iterator/simple/CountryIterator.java
@@ -0,0 +1,25 @@
+package in.deveshgupta.tutorials.designpatterns.collection.iterator.simple;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class CountryIterator implements Iterator<Country> {
+
+	private List<Country> countries;
+	private int current = -1;
+
+	public CountryIterator(List<Country> countries) {
+		this.countries = countries;
+	}
+
+	@Override
+	public boolean hasNext() {
+		return countries.size() > ++current;
+	}
+
+	@Override
+	public Country next() {
+		return countries.get(current);
+	}
+
+}
